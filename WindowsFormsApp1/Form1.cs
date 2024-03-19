731c13bca60b4e5baaf81cc84f6e744e20edb8c5
zenin5479 <zenin5479@gmail.com>
2024-03-19 23:40:19 +0300

    Update 3.9 Обновлен метод

--- WindowsFormsApp1/Form1.cs
+++ WindowsFormsApp1/Form1.cs
@@ -17,24 +17,19 @@ namespace WindowsFormsApp1
          Text = @"Определитель DNS-адресов";
          Size = new Size(400, 380);
          _onResolved = Resolved;
-
-
          Label label1 = new Label();
          label1.Parent = this;
          label1.Text = @"Введите адрес для определения:";
          label1.AutoSize = true;
          label1.Location = new Point(10, 10);
-
          _address = new TextBox();
          _address.Parent = this;
          _address.Size = new Size(200, 2 * Font.Height);
          _address.Location = new Point(10, 35);
-
          _results = new ListBox();
          _results.Parent = this;
          _results.Location = new Point(10, 65);
          _results.Size = new Size(350, 20 * Font.Height);
-
          Button checkit = new Button();
          checkit.Parent = this;
          checkit.Text = @"Определить";
@@ -48,7 +43,6 @@ namespace WindowsFormsApp1
          _results.Items.Clear();
          string addr = _address.Text;
          object state = new object();
-
          Dns.BeginGetHostEntry(addr, _onResolved, state);
          //Dns.BeginGetHostAddresses(addr, _onResolved, state);
       }
@@ -56,12 +50,9 @@ namespace WindowsFormsApp1
       private void Resolved(IAsyncResult ar)
       {
          string buffer;
-
          IPHostEntry iphe = Dns.EndGetHostEntry(ar);
-
          buffer = "Имя хоста: " + iphe.HostName;
          _results.Items.Add(buffer);
-
          foreach (string alias in iphe.Aliases)
          {
             buffer = "Псевдоним: " + alias;
