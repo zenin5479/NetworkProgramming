9a9647e2499a41722cd6c25a5f06c423b0b1d918
zenin5479 <zenin5479@gmail.com>
2024-03-20 12:06:10 +0300

    Update 4.6 Обновлен метод

--- AsyncResolve/Form1.Designer.cs
+++ AsyncResolve/Form1.Designer.cs
@@ -28,19 +28,70 @@
       /// </summary>
       private void InitializeComponent()
       {
+         ListBoxResults = new System.Windows.Forms.ListBox();
+         button1 = new System.Windows.Forms.Button();
+         TextBoxAddress = new System.Windows.Forms.TextBox();
+         label1 = new System.Windows.Forms.Label();
          SuspendLayout();
+         // 
+         // ListBoxResults
+         // 
+         ListBoxResults.FormattingEnabled = true;
+         ListBoxResults.ItemHeight = 15;
+         ListBoxResults.Location = new System.Drawing.Point(12, 41);
+         ListBoxResults.Name = "ListBoxResults";
+         ListBoxResults.Size = new System.Drawing.Size(372, 214);
+         ListBoxResults.TabIndex = 7;
+         // 
+         // button1
+         // 
+         button1.Location = new System.Drawing.Point(284, 260);
+         button1.Name = "button1";
+         button1.Size = new System.Drawing.Size(100, 25);
+         button1.TabIndex = 6;
+         button1.Text = "Определить";
+         button1.UseVisualStyleBackColor = true;
+         button1.Click += button1_Click;
+         // 
+         // TextBoxAddress
+         // 
+         TextBoxAddress.Location = new System.Drawing.Point(202, 12);
+         TextBoxAddress.Name = "TextBoxAddress";
+         TextBoxAddress.Size = new System.Drawing.Size(182, 23);
+         TextBoxAddress.TabIndex = 5;
+         TextBoxAddress.Text = "www.google.com";
+         // 
+         // label1
+         // 
+         label1.AutoSize = true;
+         label1.Location = new System.Drawing.Point(12, 15);
+         label1.Name = "label1";
+         label1.Size = new System.Drawing.Size(184, 15);
+         label1.TabIndex = 4;
+         label1.Text = "Введите адрес для определения:";
          // 
          // Form1
          // 
          AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
          AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-         ClientSize = new System.Drawing.Size(800, 450);
+         ClientSize = new System.Drawing.Size(396, 297);
+         Controls.Add(ListBoxResults);
+         Controls.Add(button1);
+         Controls.Add(TextBoxAddress);
+         Controls.Add(label1);
          Name = "Form1";
-         Text = "Form1";
+         StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+         Text = "Определитель DNS-адресов";
          Load += Form1_Load;
          ResumeLayout(false);
+         PerformLayout();
       }
 
       #endregion
+
+      private System.Windows.Forms.ListBox ListBoxResults;
+      private System.Windows.Forms.Button button1;
+      private System.Windows.Forms.TextBox TextBoxAddress;
+      private System.Windows.Forms.Label label1;
    }
 }
--- AsyncResolve/Form1.cs
+++ AsyncResolve/Form1.cs
@@ -1,5 +1,4 @@
 ﻿using System;
-using System.Drawing;
 using System.Net;
 using System.Windows.Forms;
 
@@ -7,44 +6,17 @@ namespace AsyncResolve
 {
    public sealed partial class Form1 : Form
    {
-      private readonly TextBox _address;
-      private readonly ListBox _results;
-      private readonly AsyncCallback _onResolved;
-
       public Form1()
       {
          InitializeComponent();
-         Text = @"Определитель DNS-адресов";
-         Size = new Size(363, 315);
-         _onResolved = Resolved;
-         Label label1 = new Label();
-         label1.Parent = this;
-         label1.Text = @"Введите адрес для определения:";
-         label1.AutoSize = true;
-         label1.Location = new Point(10, 10);
-         _address = new TextBox();
-         _address.Parent = this;
-         _address.Size = new Size(200, 23);
-         _address.Location = new Point(10, 35);
-         _results = new ListBox();
-         _results.Parent = this;
-         _results.Location = new Point(10, 65);
-         _results.Size = new Size(325, 200);
-         Button checkit = new Button();
-         checkit.Parent = this;
-         checkit.Text = @"Определить";
-         checkit.Location = new Point(235, 35);
-         checkit.Size = new Size(100, 23);
-         checkit.Click += ButtonResolveOnClick;
       }
 
-      void ButtonResolveOnClick(object obj, EventArgs ea)
+      private void button1_Click(object sender, EventArgs e)
       {
-         _results.Items.Clear();
-         string addr = _address.Text;
+         ListBoxResults.Items.Clear();
+         string addr = TextBoxAddress.Text;
          object state = new object();
-         Dns.BeginGetHostEntry(addr, _onResolved, state);
-         //Dns.BeginGetHostAddresses(addr, _onResolved, state);
+         Dns.BeginGetHostEntry(addr, Resolved, state);
       }
 
       private void Resolved(IAsyncResult ar)
@@ -52,16 +24,16 @@ namespace AsyncResolve
          string buffer;
          IPHostEntry iphe = Dns.EndGetHostEntry(ar);
          buffer = "Имя хоста: " + iphe.HostName;
-         _results.Items.Add(buffer);
+         ListBoxResults.Items.Add(buffer);
          foreach (string alias in iphe.Aliases)
          {
             buffer = "Псевдоним: " + alias;
-            _results.Items.Add(buffer);
+            ListBoxResults.Items.Add(buffer);
          }
          foreach (IPAddress addrs in iphe.AddressList)
          {
             buffer = "Адрес: " + addrs;
-            _results.Items.Add(buffer);
+            ListBoxResults.Items.Add(buffer);
          }
       }
 
