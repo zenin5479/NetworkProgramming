5cc5b21ca7e517dc91cee6fd36f37a6b18a1fc71
zenin5479 <zenin5479@gmail.com>
2024-03-21 16:40:54 +0300

    Update 6.3 Обновлен метод

--- WhoisAnyDomain/Form1.cs
+++ WhoisAnyDomain/Form1.cs
@@ -1,11 +1,5 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Forms;
 
 namespace WhoisAnyDomain
@@ -19,26 +13,17 @@ namespace WhoisAnyDomain
 
       private void get_BTN_Click(object sender, EventArgs e)
       {
-         List<string> whoisServers = null;
-         //разбиваем домен на уровни
-         string[] domainLevels = domainName_TB.Text.Trim().Split('.');
-         //по шагам пытаемся найти WHOIS-сервер для доменной зоны различного уровня от большей к меньшей
-         for (int a = 1; a < domainLevels.Length; a++)
+         List<string> whoisServers = new List<string>
          {
-            /*
-             * Если требуется информация по домену test.some-name.ru.com,
-             * то сначала попытаемся найти WHOIS-сервера для some-name.ru.com,
-             * после для ru.com и если всё ещё не найдём, то для com
-            */
-            string zone = string.Join(".", domainLevels, a, domainLevels.Length - a);
-            whoisServers = WhoisService.GetWhoisServers(zone);
-            //если нашли WHOIS-сервер, то поиск прекращаем
-            if (whoisServers.Count > 0)
-               break;
-         }
+            "whois.tcinet.ru",
+            "whois.ripn.net",
+            "whois.domaintools.com"
+         };
 
-         if (whoisServers == null || whoisServers.Count == 0)
-            result_TB.Text = domainName_TB.Text + "\r\n----------------\r\nНеизвестная доменная зона";
+         if (whoisServers.Count == 0)
+            result_TB.Text = domainName_TB.Text
+                             + @"---------------------------------------------------------------------"
+                             + @"Неизвестная доменная зона";
          else
          {
             result_TB.Text = "";
