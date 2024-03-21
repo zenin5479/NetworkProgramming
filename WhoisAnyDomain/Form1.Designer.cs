20e176791efac419b55e343680d452a5e718a2bb
zenin5479 <zenin5479@gmail.com>
2024-03-21 12:46:29 +0300

    Update 6.2 Обновлен метод

---
+++ WhoisAnyDomain/Form1.Designer.cs
@@ -0,0 +1,84 @@
+﻿namespace WhoisAnyDomain
+{
+   partial class Form1
+   {
+      /// <summary>
+      ///  Required designer variable.
+      /// </summary>
+      private System.ComponentModel.IContainer components = null;
+
+      /// <summary>
+      ///  Clean up any resources being used.
+      /// </summary>
+      /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+      protected override void Dispose(bool disposing)
+      {
+         if (disposing && (components != null))
+         {
+            components.Dispose();
+         }
+         base.Dispose(disposing);
+      }
+
+      #region Windows Form Designer generated code
+
+      /// <summary>
+      ///  Required method for Designer support - do not modify
+      ///  the contents of this method with the code editor.
+      /// </summary>
+      private void InitializeComponent()
+      {
+         domainName_TB = new System.Windows.Forms.TextBox();
+         get_BTN = new System.Windows.Forms.Button();
+         result_TB = new System.Windows.Forms.TextBox();
+         SuspendLayout();
+         // 
+         // domainName_TB
+         // 
+         domainName_TB.Location = new System.Drawing.Point(12, 12);
+         domainName_TB.Name = "domainName_TB";
+         domainName_TB.Size = new System.Drawing.Size(271, 23);
+         domainName_TB.TabIndex = 0;
+         domainName_TB.Text = "habrahabr.ru";
+         // 
+         // get_BTN
+         // 
+         get_BTN.Location = new System.Drawing.Point(289, 12);
+         get_BTN.Name = "get_BTN";
+         get_BTN.Size = new System.Drawing.Size(75, 23);
+         get_BTN.TabIndex = 1;
+         get_BTN.Text = "Получить!";
+         get_BTN.UseVisualStyleBackColor = true;
+         get_BTN.Click += get_BTN_Click;
+         // 
+         // result_TB
+         // 
+         result_TB.Location = new System.Drawing.Point(12, 41);
+         result_TB.Multiline = true;
+         result_TB.Name = "result_TB";
+         result_TB.ScrollBars = System.Windows.Forms.ScrollBars.Both;
+         result_TB.Size = new System.Drawing.Size(489, 397);
+         result_TB.TabIndex = 2;
+         // 
+         // Form1
+         // 
+         AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+         AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+         ClientSize = new System.Drawing.Size(513, 450);
+         Controls.Add(result_TB);
+         Controls.Add(get_BTN);
+         Controls.Add(domainName_TB);
+         Name = "Form1";
+         StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+         Text = "WHOIS";
+         ResumeLayout(false);
+         PerformLayout();
+      }
+
+      #endregion
+
+      private System.Windows.Forms.TextBox domainName_TB;
+      private System.Windows.Forms.Button get_BTN;
+      private System.Windows.Forms.TextBox result_TB;
+   }
+}
---
+++ WhoisAnyDomain/Form1.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WhoisAnyDomain
+{
+   public partial class Form1 : Form
+   {
+      public Form1()
+      {
+         InitializeComponent();
+      }
+
+      private void get_BTN_Click(object sender, EventArgs e)
+      {
+         List<string> whoisServers = null;
+         //разбиваем домен на уровни
+         string[] domainLevels = domainName_TB.Text.Trim().Split('.');
+         //по шагам пытаемся найти WHOIS-сервер для доменной зоны различного уровня от большей к меньшей
+         for (int a = 1; a < domainLevels.Length; a++)
+         {
+            /*
+             * Если требуется информация по домену test.some-name.ru.com,
+             * то сначала попытаемся найти WHOIS-сервера для some-name.ru.com,
+             * после для ru.com и если всё ещё не найдём, то для com
+            */
+            string zone = string.Join(".", domainLevels, a, domainLevels.Length - a);
+            whoisServers = WhoisService.GetWhoisServers(zone);
+            //если нашли WHOIS-сервер, то поиск прекращаем
+            if (whoisServers.Count > 0)
+               break;
+         }
+
+         if (whoisServers == null || whoisServers.Count == 0)
+            result_TB.Text = domainName_TB.Text + "\r\n----------------\r\nНеизвестная доменная зона";
+         else
+         {
+            result_TB.Text = "";
+            foreach (string whoisServer in whoisServers)
+               result_TB.Text += WhoisService.Lookup(whoisServer, domainName_TB.Text);
+         }
+      }
+   }
+}
